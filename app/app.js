10ba6f52cdc37d32f043f21468c7d7977a85e61d
matiev <matiev@live.com>
2023-06-26 19:25:13 +0300

    edit

---
+++ app/app.js
@@ -0,0 +1,24 @@
+let wListLinks = document.querySelectorAll('.wrapper__list-link');
+let wContentItems = document.querySelectorAll('.wrapper__content-item');
+
+wContentItems.forEach((item, i) => {
+  item.setAttribute('id', `tab-${i}`);
+  wListLinks[i].setAttribute('href', `#${item.getAttribute('id')}`)
+});
+
+wListLinks.forEach((item, i) => {
+  item.addEventListener('click', event => {
+    event.preventDefault();
+    let curLink = item;
+    let contId = item.getAttribute('href');
+    let curCont = document.querySelector(contId);
+    if (!curLink.closest('.wrapper__list-link--active')) {
+      wListLinks.forEach(item => item.classList.remove('wrapper__list-link--active'));
+      wContentItems.forEach(item => item.classList.remove('wrapper__content-item--show'));
+      curLink.classList.add('wrapper__list-link--active');
+      curCont.classList.add('wrapper__content-item--show');
+    }
+  });
+});
+
+wListLinks[0].click();
